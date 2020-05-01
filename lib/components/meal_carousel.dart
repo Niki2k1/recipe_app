ce09980c994af4da9351725f756ce9c4c3466627
Niklas Lausch <support@niki2k1.de>
2020-05-01 20:54:32 +0200

    Entrypoint * Removed generated leftover * Added homescreen

---
+++ lib/components/meal_carousel.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MealCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Top Meals',
+                style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5),
+              ),
+              GestureDetector(
+                onTap: () => {
+                  //TODO: Logic
+                  print('See all')
+                },
+                child: Text(
+                  'See All',
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:recipe_app/components/meal_carousel.dart';
+
+class Homescreen extends StatefulWidget {
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  int _selectedIndex = 0;
+
+  List<IconData> _icons = [
+    FontAwesomeIcons.utensils,
+    FontAwesomeIcons.hamburger,
+    FontAwesomeIcons.iceCream,
+    FontAwesomeIcons.breadSlice
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+          // TODO: Logic
+        });
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+            color: _selectedIndex == index
+                ? Theme.of(context).accentColor
+                : Color(0xFFE7EBEE),
+            borderRadius: BorderRadius.circular(30.0)),
+        child: Icon(_icons[index],
+            size: 25.0,
+            color: _selectedIndex == index
+                ? Theme.of(context).primaryColor
+                : Color(0xFFB4C1C4)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text(
+                'What would you like to eat?',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map(
+                    (MapEntry map) => _buildIcon(map.key),
+                  )
+                  .toList(),
+            ),
+            SizedBox(height: 20.0,),
+            MealCarousel(),
+          ],
+        ),
+      ),
+    );
+  }
+}
